216811bbe28d5aa9bca2a1125d2bb3093d9acb3f
koki-sys <55286703+koki-sys@users.noreply.github.com>
2022-06-18 00:41:45 +0900

    マイグレーションファイルの追加

---
+++ src/database/migrations/2022_06_17_143450_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * 投稿テーブルの情報を入れてます。
+     *
+     * @author takahashi koki
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('port_url');
+            $table->string('class');
+            $table->string('git_url');
+            $table->string('comment');
+            $table->string('use_language');
+            $table->string('img_url');
+            $table->timestamps();
+
+            // 外部キーの設定
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
