fc10ee2e03d89605901d4490b6dacb115275cb86
koki-sys <55286703+koki-sys@users.noreply.github.com>
2022-06-18 13:03:46 +0900

    Modelの作成

--- src/app/Post.php
+++ src/app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /**
+     * 複数代入しない属性
+     * コントローラーでエラー発生防止
+     *
+     * @var array
+     */
+    protected $guarded = [];
 }
