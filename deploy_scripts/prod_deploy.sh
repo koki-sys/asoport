bd81ec23606f08b108bacc2a04a430e78ff16a8d
koki-sys <55286703+koki-sys@users.noreply.github.com>
2022-06-30 23:15:53 +0900

    不要ファイルの削除

--- deploy_scripts/prod_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/service-user/projects/emovee
-# ビルド時にパーミッションエラーになるため
-chmod -R 777 docker/mysql/data
-
-# イメージビルド&コンテナ起動
-docker-compose down
-docker-compose build
-docker-compose up -d
