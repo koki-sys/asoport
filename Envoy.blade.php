bb31b7fc0c614c50ddd7719d806c041eb4f0bc59
koki-sys <55286703+koki-sys@users.noreply.github.com>
2022-07-07 01:05:06 +0900

    デプロイワークフローの修正

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -6,13 +6,13 @@
     cd ~ {{-- ここは環境に合わせて変更する --}}
     git clone git@github.com:koki-sys/asoport.git
     cd asoport
-    docker compose down
-    docker compose up -d --build
-    docker compose exec app composer install
-    docker compose exec app npm install
-    docker compose exec app cp .env.example .env
-    docker compose exec app php artisan key:generate
-    docker compose exec app php artisan storage:link
-    docker compose exec app chmod -R 777 storage bootstrap/cache
-    docker compose exec app php artisan migrate:fresh --seed
+    docker-compose down
+    docker-compose up -d --build
+    docker-compose exec app composer install
+    docker-compose exec app npm install
+    docker-compose exec app cp .env.example .env
+    docker-compose exec app php artisan key:generate
+    docker-compose exec app php artisan storage:link
+    docker-compose exec app chmod -R 777 storage bootstrap/cache
+    docker-compose exec app php artisan migrate:fresh --seed
 @endtask
