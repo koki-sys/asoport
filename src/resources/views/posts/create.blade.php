74ba310e4fa2d156dfac2b3369d6f50692a9c366
koki-sys <soraisu7@gmail.com>
2022-07-16 23:25:07 +0900

    バリデーション一部作成

--- src/resources/views/posts/create.blade.php
+++ src/resources/views/posts/create.blade.php
@@ -24,11 +24,11 @@
                             <div id="previewArea"></div>
                         </div>
                     </div>
-                    <input type="text" name="name" placeholder="名前" class="input-field"><br>
-                    <input type="text" name="class" placeholder="学科" class="input-field"><br>
-                    <input type="email" name="mail" placeholder="メールアドレス" class="input-field"><br>
-                    <input type="text" name="port" placeholder="ポートフォリオサイトのURL" class="input-field"><br>
-                    <input type="text" name="git" placeholder="GitHubのURL" class="input-field"><br>
+                    <input type="text" name="name" placeholder="名前" class="input-field required"><br>
+                    <input type="text" name="class" placeholder="学科" class="input-field required"><br>
+                    <input type="email" name="mail" placeholder="メールアドレス" class="input-field required"><br>
+                    <input type="text" name="port" placeholder="ポートフォリオサイトのURL" class="input-field required"><br>
+                    <input type="text" name="git" placeholder="GitHubのURL" class="input-field required"><br>
                     <div class="lang-box">
                         <p>使用言語</p>
                         <label class="lang-label"><input type="checkbox" name="language" value="html"><span>HTML</span></label>
@@ -37,7 +37,7 @@
                         <label class="lang-label"><input type="checkbox" name="language" value="java"><span>JAVA</span></label>
                         <label class="lang-label"><input type="checkbox" name="language" value="javascript"><span>JAVASCRIPT</span></label>
                     </div>
-                    <input type="text" name="comment" placeholder="ひとこと" class="input-field"><br>
+                    <input type="text" name="comment" placeholder="ひとこと" class="input-field required"><br>
                     <button type="submit" class="post-btn btn btn-lg">投稿する</button>
                 </div>
             </form>
@@ -47,4 +47,5 @@
     </div>
 </div>
 <script src="{{ mix('js/dragDrop.js') }}"></script>
+<script src="{{ mix('js/createValidation.js') }}"></script>
 @endsection('content')
