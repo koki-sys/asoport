2938f5cf909351430a28365d0e7735b824cea32b
koki-sys <55286703+koki-sys@users.noreply.github.com>
2022-09-16 11:41:41 +0900

    コード整形

--- src/app/Http/Controllers/detailController.php
+++ src/app/Http/Controllers/detailController.php
@@ -26,15 +26,16 @@ class detailController extends Controller
      */
     public function index(Request $request)
     {
-        function getParam($key, $pattern, $error){
+        function getParam($key, $pattern, $error)
+        {
             //$_POST['foo']の代わり。配列チェック
-            $val = filter_input(INPUT_POST , $key);
+            $val = filter_input(INPUT_POST, $key);
             //文字エンコーディング(UTF-8)のチェック
-            if(! mb_check_encoding($val, 'UTF-8')) {
+            if (!mb_check_encoding($val, 'UTF-8')) {
                 die('文字エンコーディングが不正です');
             }
             //正規表現とのチェック
-            if (preg_match($pattern, $val) !==1) {
+            if (preg_match($pattern, $val) !== 1) {
                 die($error);
             }
             return $val;
@@ -42,15 +43,13 @@ class detailController extends Controller
 
 
         $id = $request->input('id');
-        $userid = getParam($id, '/\A[[[:^cntrl:]][0-9]\z/','選択されたデータを読み取れませんでした。');
+        $userid = getParam($id, '/\A[[[:^cntrl:]][0-9]\z/', '選択されたデータを読み取れませんでした。');
         $detail = post::find(htmlspecialchars($id, ENT_COMPAT, "UTF-8"));
 
-        if($detail){
-            return view('welcome',compact($detail));
+        if ($detail) {
+            return view('welcome', compact($detail));
         } else {
             die('選択されたデータを読み取れませんでした。');
         }
-
     }
-
 }
