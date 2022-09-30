601a6d63532b2bed7d4ea30cbb3bf16a34bfffa9
2101043 <tatsuya.1221.12@ezweb.ne.jp>
2022-09-30 10:09:15 +0900

    Update welcome.blade.php

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
 
     <link rel="stylesheet" href="css/style.css">
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="script/masonry.pkgd.min.js"></script>
 </head>
 
@@ -26,35 +27,35 @@
             </header>
         </div>
     @else
-        <div class="header_wrapper">
-            <header class="c_flex_center">
-                <h1 class="c_pacifico">ASOPort</h1>
-                <div class="search_wrapper">
-                    <ion-icon class="search_icon" name="search-outline"></ion-icon>
-                    <ion-icon class="close_icon" name="close-outline"></ion-icon>
-                </div>
-            </header>
-        </div>
-
+    <div class="header_wrapper">
+        <header class="c_flex_center">
+            <h1 class="c_pacifico">ASOPort</h1>
+            <div class="search_wrapper">
+                <ion-icon class="search_icon" name="search-outline"></ion-icon>
+                <ion-icon class="close_icon" name="close-outline"></ion-icon>
+            </div>
+        </header>
+    </div>
+    @endif
     <!-- ===== Search Field ===== -->
     <div class="search_field c_font_bold">
-        <form action="/search" method="post">
-            @csrf
-            <div class="search_input_wrapper c_font_bold">
-                <label class="" for="keyword">キーワード検索</label>
-                <input class="c_font_regular" type="text" name="keyword" id="keyword">
-                <label class="">使用言語</label>
-                <div class="checkbox_wrapper">
-                    <input type="checkbox" name="language[]" value="HTML" id="html"><label for="html">HTML</label>
-                    <input type="checkbox" name="language[]" value="CSS" id="css"><label for="css">CSS</label>
-                    <input type="checkbox" name="language[]" value="JavaScript" id="javascript"><label for="javascript">JavaScript</label>
-                    <input type="checkbox" name="language[]" value="PHP" id="php"><label for="php">PHP</label>
-                    <input type="checkbox" name="language[]" value="Java" id="java"><label for="java">Java</label>
-                </div>
+        <!-- <form action="" method=""> -->
+        <div class="search_input_wrapper c_font_bold">
+            <label class="" for="keyword">キーワード検索</label>
+            <input class="c_font_regular" type="text" name="keyword" id="keyword">
 
-                <button class="c_font_bold">検索する</button>
+            <label class="">使用言語</label>
+            <div class="checkbox_wrapper">
+                <input type="checkbox" name="html" id="html"><label for="html">HTML</label>
+                <input type="checkbox" name="css" id="css"><label for="css">CSS</label>
+                <input type="checkbox" name="javascript" id="javascript"><label for="javascript">JavaScript</label>
+                <input type="checkbox" name="php" id="php"><label for="php">PHP</label>
+                <input type="checkbox" name="java" id="java"><label for="java">Java</label>
             </div>
-        </form>
+
+            <button class="c_font_bold">検索する</button>
+        </div>
+        <!-- </form> -->
     </div>
     <!-- ===== 検索フィールド表示時の黒背景 ===== -->
     <div class="search_black_back"></div>
@@ -66,7 +67,7 @@
         <div class="portfolio">
             <div class="portfolio_background">
                 <div class="portfolio_img">
-                    <img src="{{ asset($post -> img_url) }}" alt="" />
+                    <img src="{{ $post -> img_url}}" alt="" />
                     <div class="img_hover_style c_font_bold">
                         <ion-icon name="camera-outline"></ion-icon>
                         詳細を見る
