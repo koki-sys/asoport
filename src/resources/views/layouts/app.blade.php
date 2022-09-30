8e38abf3ef1d88ab361272ceac73b7c21ab9147c
koki-sys <55286703+koki-sys@users.noreply.github.com>
2022-09-30 12:16:14 +0900

    welcomeページの修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -2,10 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ASOPort</title>
 
-    <title>Laravel</title>
+    <link rel="stylesheet" href="css/style.css">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -13,7 +14,6 @@
     <!-- font awesome -->
     <script src="https://kit.fontawesome.com/9ddb128e39.js" crossorigin="anonymous"></script>
 
-    <!-- jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="script/masonry.pkgd.min.js"></script>
 
@@ -22,8 +22,56 @@
 </head>
 
 <body class="light">
+    <!-- ====== Header ====== -->
+    @if(Auth::check())
+    <div class="header_wrapper">
+        <header class="c_flex_center">
+            <h1 class="c_pacifico"><a href="{{ url('/') }}">ASOPort</a></h1>
+            <div class="search_wrapper">
+                <ion-icon class="search_icon" name="search-outline"></ion-icon>
+                <ion-icon class="close_icon" name="close-outline"></ion-icon>
+            </div>
+            <div class="header_icon_wrapper">
+                <a href="{{ url('/create') }}">
+                    <ion-icon class="plus_icon" id="add_portfolio" name="add-outline"></ion-icon>
+                </a>
+                <ion-icon class="open_menu_icon" name="person-outline"></ion-icon>
+                <ion-icon class="theme_toggle" name="contrast-outline"></ion-icon>
+            </div>
+            <div class="menu">
+                <a class="c_font_bold" href="{{ url('/profile') }}">プロフィールへ</a>
+                <a class="c_font_bold" href="#">プロフィールを編集</a>
+                <!-- ↓ログイン後はログアウトにしてください -->
+                <a class="c_font_bold" href="#">ログアウト</a>
+            </div>
+        </header>
+    </div>
+    @else
+    <div class="header_wrapper">
+        <header class="c_flex_center">
+            <h1 class="c_pacifico"><a href="{{ url('/') }}">ASOPort</a></h1>
+            <div class="search_wrapper">
+                <ion-icon class="search_icon" name="search-outline"></ion-icon>
+                <ion-icon class="close_icon" name="close-outline"></ion-icon>
+            </div>
+            <div class="header_icon_wrapper">
+                <ion-icon class="open_menu_icon" name="person-outline"></ion-icon>
+                <ion-icon class="theme_toggle" name="contrast-outline"></ion-icon>
+            </div>
+            <div class="menu">
+                <a class="c_font_bold" href="{{ url('/register') }}">新規登録へ</a>
+                <a class="c_font_bold" href="{{url('/login') }}">ログイン</a>
+            </div>
+        </header>
+    </div>
+    @endif
+
+    <!-- それぞれのページの中身をインポート -->
     @yield('content')
+
     <script src="script/script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,124 +1,62 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ASOPort</title>
+@section('content')
+<!-- ===== Search Field ===== -->
+<div class="search_field c_font_bold">
+    <!-- <form action="" method=""> -->
+    <div class="search_input_wrapper c_font_bold">
+        <label class="" for="keyword">キーワード検索</label>
+        <input class="c_font_regular" type="text" name="keyword" id="keyword">
 
-    <link rel="stylesheet" href="css/style.css">
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="script/masonry.pkgd.min.js"></script>
-</head>
-
-<body class="light">
-    <!-- ====== Header ====== -->
-    @if(Auth::check())
-    <div class="header_wrapper">
-        <header class="c_flex_center">
-            <h1 class="c_pacifico"><a href="{{ url('/') }}">ASOPort</a></h1>
-            <div class="search_wrapper">
-                <ion-icon class="search_icon" name="search-outline"></ion-icon>
-                <ion-icon class="close_icon" name="close-outline"></ion-icon>
-            </div>
-            <div class="header_icon_wrapper">
-                <a href="{{ url('/create') }}">
-                    <ion-icon class="plus_icon" id="add_portfolio" name="add-outline"></ion-icon>
-                </a>
-                <ion-icon class="open_menu_icon" name="person-outline"></ion-icon>
-                <ion-icon class="theme_toggle" name="contrast-outline"></ion-icon>
-            </div>
-            <div class="menu">
-                <a class="c_font_bold" href="{{ url('/profile') }}">プロフィールへ</a>
-                <a class="c_font_bold" href="#">プロフィールを編集</a>
-                <!-- ↓ログイン後はログアウトにしてください -->
-                <a class="c_font_bold" href="#">ログアウト</a>
-            </div>
-        </header>
-    </div>
-    @else
-    <div class="header_wrapper">
-        <header class="c_flex_center">
-            <h1 class="c_pacifico"><a href="{{ url('/') }}">ASOPort</a></h1>
-            <div class="search_wrapper">
-                <ion-icon class="search_icon" name="search-outline"></ion-icon>
-                <ion-icon class="close_icon" name="close-outline"></ion-icon>
-            </div>
-            <div class="header_icon_wrapper">
-                <ion-icon class="open_menu_icon" name="person-outline"></ion-icon>
-                <ion-icon class="theme_toggle" name="contrast-outline"></ion-icon>
-            </div>
-            <div class="menu">
-                <a class="c_font_bold" href="{{ url('/register') }}">新規登録へ</a>
-                <a class="c_font_bold" href="{{url('/login') }}">ログイン</a>
-            </div>
-        </header>
-    </div>
-    @endif
-    <!-- ===== Search Field ===== -->
-    <div class="search_field c_font_bold">
-        <!-- <form action="" method=""> -->
-        <div class="search_input_wrapper c_font_bold">
-            <label class="" for="keyword">キーワード検索</label>
-            <input class="c_font_regular" type="text" name="keyword" id="keyword">
-
-            <label class="">使用言語</label>
-            <div class="checkbox_wrapper">
-                <input type="checkbox" name="html" id="html"><label for="html">HTML</label>
-                <input type="checkbox" name="css" id="css"><label for="css">CSS</label>
-                <input type="checkbox" name="javascript" id="javascript"><label for="javascript">JavaScript</label>
-                <input type="checkbox" name="php" id="php"><label for="php">PHP</label>
-                <input type="checkbox" name="java" id="java"><label for="java">Java</label>
-            </div>
-
-            <button class="c_font_bold">検索する</button>
+        <label class="">使用言語</label>
+        <div class="checkbox_wrapper">
+            <input type="checkbox" name="html" id="html"><label for="html">HTML</label>
+            <input type="checkbox" name="css" id="css"><label for="css">CSS</label>
+            <input type="checkbox" name="javascript" id="javascript"><label for="javascript">JavaScript</label>
+            <input type="checkbox" name="php" id="php"><label for="php">PHP</label>
+            <input type="checkbox" name="java" id="java"><label for="java">Java</label>
         </div>
-        <!-- </form> -->
-    </div>
-    <!-- ===== 検索フィールド表示時の黒背景 ===== -->
-    <div class="search_black_back"></div>
 
-    <!-- ===== Portfolio List ===== -->
-    <div class="portfolio_list">
-        <!-- ===== ポートフォリオの一塊 ===== -->
-        @foreach($posts as $post)
-        <div class="portfolio">
-            <div class="portfolio_background">
-                <div class="portfolio_img">
-                    <img src="{{ asset($post -> img_url) }}" alt="" />
-                    <div class="img_hover_style c_font_bold">
-                        <ion-icon name="camera-outline"></ion-icon>
-                        詳細を見る
-                    </div>
+        <button class="c_font_bold">検索する</button>
+    </div>
+    <!-- </form> -->
+</div>
+<!-- ===== 検索フィールド表示時の黒背景 ===== -->
+<div class="search_black_back"></div>
+
+<!-- ===== Portfolio List ===== -->
+<div class="portfolio_list">
+    <!-- ===== ポートフォリオの一塊 ===== -->
+    @foreach($posts as $post)
+    <div class="portfolio">
+        <div class="portfolio_background">
+            <div class="portfolio_img">
+                <img src="{{ asset($post -> img_url) }}" alt="" />
+                <div class="img_hover_style c_font_bold">
+                    <ion-icon name="camera-outline"></ion-icon>
+                    詳細を見る
                 </div>
-                <div class="content">
-                    <p><a href="{{ $post -> port_url }}" class="portfolio_link">{{ $post -> port_url }}</a></p>
-                    <h1 class="c_font_bold">
-                        {{ $post -> name }}<span>{{ $post -> class }}</span>
-                    </h1>
-                    <h3>
-                        <a href="{{ $post -> git_url }}" class="portfolio_link">
-                            <ion-icon name="logo-github"></ion-icon>
-                            GitHub <span>{{ $post -> git_url }}</span>
-                        </a>
-                    </h3>
-                    <h2>{{ $post -> comment }}</h2>
-                    <h4>
-                        <p>使用言語</p>
-                        {{ $post -> use_language }}
-                    </h4>
-                </div>
-                <ion-icon class="portfolio_close" name="arrow-back-outline"></ion-icon>
             </div>
+            <div class="content">
+                <p><a href="{{ $post -> port_url }}" class="portfolio_link">{{ $post -> port_url }}</a></p>
+                <h1 class="c_font_bold">
+                    {{ $post -> name }}<span>{{ $post -> class }}</span>
+                </h1>
+                <h3>
+                    <a href="{{ $post -> git_url }}" class="portfolio_link">
+                        <ion-icon name="logo-github"></ion-icon>
+                        GitHub <span>{{ $post -> git_url }}</span>
+                    </a>
+                </h3>
+                <h2>{{ $post -> comment }}</h2>
+                <h4>
+                    <p>使用言語</p>
+                    {{ $post -> use_language }}
+                </h4>
+            </div>
+            <ion-icon class="portfolio_close" name="arrow-back-outline"></ion-icon>
         </div>
-        @endforeach
     </div>
-
-    <script src="script/script.js"></script>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-</body>
-
-</html>
+    @endforeach
+</div>
+@endsection
