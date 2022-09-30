f53e076f0ba2085aa1a9179646fdd09ece423a28
koki-sys <55286703+koki-sys@users.noreply.github.com>
2022-09-30 20:33:19 +0900

    ログインページのスタイルの修正

---
+++ src/resources/views/layouts/nosearch.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ASOPort</title>
+
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+</head>
+</head>
+
+<body class="light">
+    <!-- ====== Header ====== -->
+    @if(Auth::check())
+    <div class="header_wrapper">
+        <header class="c_flex_center">
+            <h1 class="c_pacifico"><a href="{{ url('/') }}">ASOPort</a></h1>
+            <div class="header_icon_wrapper">
+                <a href="{{ url('/create') }}">
+                    <ion-icon class="plus_icon" id="add_portfolio" name="add-outline"></ion-icon>
+                </a>
+                <ion-icon class="open_menu_icon" name="person-outline"></ion-icon>
+                <ion-icon class="theme_toggle" name="contrast-outline"></ion-icon>
+            </div>
+            <div class="menu">
+                <a class="c_font_bold" href="{{ url('/profile') }}">プロフィールへ</a>
+                <a class="c_font_bold" href="#">プロフィールを編集</a>
+                <!-- 要修正(css) -->
+                <a>
+                    <form action="{{ url('/logout') }}" method="POST">
+                        @csrf
+                        <input type="submit" value="ログアウト" class="c_font_bold border border-0">
+                    </form>
+                </a>
+            </div>
+        </header>
+    </div>
+    @else
+    <div class="header_wrapper">
+        <header class="c_flex_center">
+            <h1 class="c_pacifico"><a href="{{ url('/') }}">ASOPort</a></h1>
+            <div class="header_icon_wrapper">
+                <ion-icon class="open_menu_icon" name="person-outline"></ion-icon>
+                <ion-icon class="theme_toggle" name="contrast-outline"></ion-icon>
+            </div>
+            <div class="menu">
+                <a class="c_font_bold" href="{{ url('/register') }}">新規登録へ</a>
+                <a class="c_font_bold" href="{{ url('/login') }}">ログイン</a>
+            </div>
+        </header>
+    </div>
+    @endif
+
+    <!-- それぞれのページの中身をインポート -->
+    @yield('content')
+
+    <script src="script/script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
