25b4ec753cf442acbc4cc28ebffe0abee6ee98fd
koki-sys <55286703+koki-sys@users.noreply.github.com>
2022-10-07 23:48:23 +0900

    トップ画面の処理一部修正

--- src/app/Http/Controllers/TopController.php
+++ src/app/Http/Controllers/TopController.php
@@ -16,7 +16,7 @@ class TopController extends Controller
      */
     public function index(Request $request)
     {
-        $posts = Post::select('users.name', 'posts.*')
+        $posts = Post::select('users.name', 'users.class', 'posts.*')
             ->join('users', 'users.id', '=', 'posts.user_id')
             ->get();
 
