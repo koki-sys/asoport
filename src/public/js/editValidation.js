7b408235f33f261ff33ef26d84d2b6cfa6939663
koki-sys <soraisu7@gmail.com>
2022-10-10 14:04:38 +0900

    プロフィール編集のバリデーション機能の追加

---
+++ src/public/js/editValidation.js
@@ -0,0 +1,73 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************************!*\
+  !*** ./resources/js/editValidation.js ***!
+  \****************************************/
+document.addEventListener('DOMContentLoaded', function () {
+  //.validationForm を指定した最初の form 要素を取得
+  var validationForm = document.querySelector('.validationForm'); //.validationForm を指定した form 要素が存在すれば
+
+  if (validationForm) {
+    //エラーを表示する span 要素に付与するクラス名（エラー用のクラス）
+    var errorClassName = 'error'; //required クラスを指定された要素の集まり
+
+    var requiredElems = document.querySelectorAll('.required'); //email クラスを指定された要素の集まり
+
+    var emailElems = document.querySelectorAll('.email'); //エラーメッセージを表示する span 要素を生成して親要素に追加する関数
+    //elem ：対象の要素
+    //errorMessage ：表示するエラーメッセージ
+
+    var createError = function createError(elem, errorMessage) {
+      //span 要素を生成
+      var errorSpan = document.createElement('span'); //エラー用のクラスを追加（設定）
+
+      errorSpan.classList.add(errorClassName); //aria-live 属性を設定
+
+      errorSpan.setAttribute('aria-live', 'polite'); //引数に指定されたエラーメッセージを設定
+
+      errorSpan.textContent = errorMessage; //elem の親要素の子要素として追加
+
+      elem.parentNode.insertBefore(errorSpan, elem.nextSibling);
+      console.log("error:" + errorMessage);
+    }; //form 要素の submit イベントを使った送信時の処理
+
+
+    validationForm.addEventListener('submit', function (e) {
+      //エラーを表示する要素を全て取得して削除（初期化）
+      var errorElems = validationForm.querySelectorAll('.' + errorClassName);
+      errorElems.forEach(function (elem) {
+        elem.remove();
+      }); //.required を指定した要素を検証
+
+      requiredElems.forEach(function (elem) {
+        var elemValue = elem.value.trim(); //値が空の場合はエラーを表示してフォームの送信を中止
+
+        if (elem.getAttribute('type') === 'file' && elemValue.length === 0) {
+          createError(document.querySelector('#dragDropArea'), '入力は必須です');
+        } else if (elemValue.length === 0) {
+          createError(elem, '入力は必須です');
+          e.preventDefault();
+        }
+      }); //.email を指定した要素を検証
+
+      emailElems.forEach(function (elem) {
+        //Email の検証に使用する正規表現パターン
+        var pattern = /^([\+\x2D0-9_a-z\u017F\u212A]+)(\.[\+\x2D0-9_a-z\u017F\u212A]+)*@([\x2D0-9a-z\u017F\u212A]+\.)+[a-z\u017F\u212A]{2,6}$/i; //値が空でなければ
+
+        if (elem.value !== '') {
+          //test() メソッドで値を判定し、マッチしなければエラーを表示してフォームの送信を中止
+          if (!pattern.test(elem.value)) {
+            createError(elem, 'Email アドレスの形式が正しくないようです。');
+            e.preventDefault();
+          }
+        }
+      });
+      window.scroll({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  }
+});
+/******/ })()
+;
