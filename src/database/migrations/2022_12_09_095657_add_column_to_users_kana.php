d9a951b0eff3f5b7cda8b8ac4c8366aee4f2bf74
nakagawa0000 <91577844+nakagawa0000@users.noreply.github.com>
2022-12-09 10:28:40 +0900

    データベース修正

--- src/database/migrations/2022_12_09_095657_add_column_to_users_kana.php
+++ src/database/migrations/2022_12_09_095657_add_column_to_users_kana.php
@@ -14,7 +14,7 @@ class AddColumnToUsersKana extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('kana');
+            $table->string('kana')->nullable();
         });
     }
 
