9e2e05bef78cc2783a849fba717a42e5f2d34bec
koki-sys <soraisu7@gmail.com>
2022-12-09 19:26:52 +0900

    フローの修正

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,11 +1,20 @@
 {{-- AWS EC2へデプロイする予定なので、接続先を記述 --}}
 @servers(['web' => ['ec2-user@15.168.21.10']])
+@servers(['testweb' => ['ec2-user@44.197.127.22']])
 
 {{-- serversで記述した「web」のサーバーを対象に、taskで囲った範囲のコマンドを[foo]として定義して実行する --}}
 @task('deploy', ['on' => 'web'])
-    cd asoport {{-- ここは環境に合わせて変更する --}}
-    echo 'pulling......'
-    git pull git@github.com:koki-sys/asoport.git
-    echo 'pulled.'
-    make prod
+cd asoport {{-- ここは環境に合わせて変更する --}}
+echo 'pulling......'
+git pull git@github.com:koki-sys/asoport.git
+echo 'pulled.'
+make prod
+@endtask
+
+@task('testdeploy', ['on' => 'testweb'])
+cd asoport {{-- ここは環境に合わせて変更する --}}
+echo 'pulling......'
+git pull git@github.com:koki-sys/asoport.git
+echo 'pulled.'
+make init
 @endtask
