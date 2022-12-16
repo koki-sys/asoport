0e29af803513617e446f00a907ac0c6378878480
2101043 <tatsuya.1221.12@ezweb.ne.jp>
2022-12-16 12:22:07 +0900

    Create PostDeleteController.php

---
+++ src/app/Http/Controllers/PostDeleteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+class PostDeleteController extends Controller
+{
+    public function postdelete(Request $request)
+    {
+        $id = $request->input('id');
+
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect('/profile');
+    }
+}
