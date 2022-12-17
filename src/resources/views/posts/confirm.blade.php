d5c2d0e5921901cf329269833cc49ff804fa7515
koki-sys <soraisu7@gmail.com>
2022-12-17 13:07:48 +0900

    マージン等の修正

--- src/resources/views/posts/confirm.blade.php
+++ src/resources/views/posts/confirm.blade.php
@@ -4,7 +4,8 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="script/masonry.pkgd.min.js"></script>
 @endsection
 
@@ -12,22 +13,14 @@
 <div class="container mt-2">
     <div class="row">
         <div class="col-md-2"></div>
-        <div class="col-md-8">
-            <a href="javascript:history.back();" class="back-box d-flex align-items-center">
-                <ion-icon name="arrow-back-outline" style="font-size: 1.6rem" class="mr-2"></ion-icon>
-                <span style="font-size: 1.2rem">戻る</span>
-            </a>
-        </div>
-        <div class="col-md-2"></div>
-    </div>
-    <div class="row">
-        <div class="col-md-2"></div>
         <div class="col-md-8 mt-5">
-            <div class="mb-3 text-center"><span style="font-size: 1.5rem; font-weight: bold;">内容を確認してください</span></div>
-            <div class="mb-3 align-middle d-inline-block">
-            
+            <div class="mb-5 text-center">
+                <a href="javascript:history.back();" class="back-box d-flex align-items-center">
+                    <ion-icon name="arrow-back-outline" style="font-size: 1.6rem" class="mr-2"></ion-icon>
+                    <span style="font-size: 1.2rem">戻る</span>
+                </a>
+                <span style="font-size: 1.5rem; font-weight: bold;">内容を確認してください</span>
             </div>
-            
             <form method="post" enctype="multipart/form-data" action="{{ $action }}" class="validationForm" novalidate>
                 @csrf
                 <input type="hidden" name="id" value="{{ $input_data['id'] }}">
@@ -38,19 +31,24 @@
                         <input name="img_url" value="{{$img_url}}" type="hidden">
                     </div>
                     <div class="m-3 pt-3">
-                    <div class="text-left"><span style="font-size: 1.1rem; font-weight: bold;" >ポートフォリオサイトURL</span><span class="pl-4">{{$input_data['port']}}</span></div>
+                        <div class="text-left"><span
+                                style="font-size: 1.1rem; font-weight: bold;">ポートフォリオサイトURL</span><span
+                                class="pl-4">{{$input_data['port']}}</span></div>
                         <input name="port" value="{{$input_data['port']}}" type="hidden">
                     </div>
                     <div class="m-3 pt-2">
-                    <div class="text-left"><span style="font-size: 1.1rem; font-weight: bold;" >GitHub URL</span><span class="pl-4">{{$input_data['git']}}</span></div>
+                        <div class="text-left"><span style="font-size: 1.1rem; font-weight: bold;">GitHub
+                                URL</span><span class="pl-4">{{$input_data['git']}}</span></div>
                         <input name="git" value="{{$input_data['git']}}" type="hidden">
                     </div>
                     <div class="m-3 pt-2">
-                    <div class="text-left"><span style="font-size: 1.1rem; font-weight: bold;" >使用技術</span><span class="pl-4">{{$language}}</span></div>
+                        <div class="text-left"><span style="font-size: 1.1rem; font-weight: bold;">使用技術</span><span
+                                class="pl-4">{{$language}}</span></div>
                         <input name="lang" value="{{ $language }}" type="hidden">
                     </div>
                     <div class="m-3 pt-2">
-                    <div class="text-left"><span style="font-size: 1.1rem; font-weight: bold;" >ひとこと</span><span class="pl-4">{{ $input_data['comment']}}</span></div>
+                        <div class="text-left"><span style="font-size: 1.1rem; font-weight: bold;">ひとこと</span><span
+                                class="pl-4">{{ $input_data['comment']}}</span></div>
                         <input name="comment" value="{{$input_data['comment']}}" type="hidden">
                     </div>
                     <button type="submit" class="post-btn btn btn-lg">{{ $btn_title }}</button>
