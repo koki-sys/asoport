050be4804105698d03becbda5442b2503adc6284
koki-sys <soraisu7@gmail.com>
2022-12-29 14:11:47 +0900

    テストデータの修正

--- src/database/seeds/PostsTableSeeder.php
+++ src/database/seeds/PostsTableSeeder.php
@@ -20,7 +20,8 @@ class PostsTableSeeder extends Seeder
                 'git_url' => Str::random(10) . '/github.com',
                 'comment' => 'ポートフォリオサイトを作りました。 ご覧いただけますと幸いです。',
                 'use_language' => 'PHP',
-                'img_url' => 'img/200x200.png'
+                'img_url' => 'img/200x200.png',
+                'public_flg' => 1
             ],
             [
                 'user_id' => 1,
@@ -28,7 +29,26 @@ class PostsTableSeeder extends Seeder
                 'git_url' => Str::random(10) . '/github.com',
                 'comment' => 'ご覧いただけますと幸いです。',
                 'use_language' => 'JavaScript / HTML / CSS',
-                'img_url' => 'img/500x750.png'
+                'img_url' => 'img/500x750.png',
+                'public_flg' => 1
+            ],
+            [
+                'user_id' => 1,
+                'port_url' => Str::random(10) . '/example.com',
+                'git_url' => Str::random(10) . '/github.com',
+                'comment' => 'こんにちは',
+                'use_language' => 'React / JavaScript / PHP / HTML',
+                'img_url' => 'img/1500x1000.png',
+                'public_flg' => 0
+            ],
+            [
+                'user_id' => 1,
+                'port_url' => Str::random(10) . '/example.com',
+                'git_url' => Str::random(10) . '/github.com',
+                'comment' => 'チームで開発しました。',
+                'use_language' => 'PHP / HTML / Laravel',
+                'img_url' => 'img/1980x1440.png',
+                'public_flg' => 0
             ],
             [
                 'user_id' => 2,
@@ -36,7 +56,8 @@ class PostsTableSeeder extends Seeder
                 'git_url' => Str::random(10) . '/github.com',
                 'comment' => 'ポートフォリオサイトを作りました。',
                 'use_language' => 'JavaScript / PHP / HTML',
-                'img_url' => 'img/1500x1000.png'
+                'img_url' => 'img/1500x1000.png',
+                'public_flg' => 1
             ],
             [
                 'user_id' => 2,
@@ -44,7 +65,26 @@ class PostsTableSeeder extends Seeder
                 'git_url' => Str::random(10) . '/github.com',
                 'comment' => 'こんにちは',
                 'use_language' => 'PHP / HTML',
-                'img_url' => 'img/1980x1440.png'
+                'img_url' => 'img/1980x1440.png',
+                'public_flg' => 1
+            ],
+            [
+                'user_id' => 2,
+                'port_url' => Str::random(10) . '/example.com',
+                'git_url' => Str::random(10) . '/github.com',
+                'comment' => 'こんにちは',
+                'use_language' => 'React / JavaScript / PHP / HTML',
+                'img_url' => 'img/1500x1000.png',
+                'public_flg' => 0
+            ],
+            [
+                'user_id' => 2,
+                'port_url' => Str::random(10) . '/example.com',
+                'git_url' => Str::random(10) . '/github.com',
+                'comment' => 'チームで開発しました。',
+                'use_language' => 'PHP / HTML / Laravel',
+                'img_url' => 'img/1980x1440.png',
+                'public_flg' => 0
             ],
         ];
         DB::table('posts')->insert($data);
