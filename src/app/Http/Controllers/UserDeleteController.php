0c32e05bbd7e3787939d87587637b6a5d622766a
koki-sys <soraisu7@gmail.com>
2023-01-12 13:53:09 +0900

    アカウント削除処理の修正

--- src/app/Http/Controllers/UserDeleteController.php
+++ src/app/Http/Controllers/UserDeleteController.php
@@ -13,19 +13,12 @@ class UserDeleteController extends Controller
     public function userdelete(Request $request)
     {
         $user = User::find(auth::id());
-        
-        $post = Post::where('user_id', $user->id)->delete();
+
+        Post::where('user_id', $user->id)->delete();
 
         //ユーザ削除
         $user->delete();
-       
 
         return redirect('/');
-
-        //ユーザが投稿したデータ削除
-       $post = Post::where('user_id', $user->id)->delete();
-
-       return redirect('/');
-
     }
 }
